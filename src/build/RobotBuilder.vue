762be5bece64e54069b057a4c89e0a16534da217
sajeevah <sa.hasith@gmail.com>
2020-04-01 22:52:20 +0530

    mixins

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -58,6 +58,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -71,9 +72,6 @@ function getNextValidIndex(index, length) {
 
 export default {
   name: 'RobotBuilder',
-  created() {
-    console.log('component created');
-  },
   data() {
     return {
       availableParts,
@@ -85,6 +83,7 @@ export default {
       cart: [],
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
